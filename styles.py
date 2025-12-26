734b2d013eaa6fa16bee4c94519d410815b3327a
Chaitanya081 <192424185.simats@saveetha.com>
2025-12-26 21:02:29 +0530

    styles.py

--- styles.py
+++ styles.py
@@ -2,22 +2,22 @@ import streamlit as st
 import base64
 
 def apply_login_style(image_path):
-    with open(image_path, "rb") as img_file:
-        encoded = base64.b64encode(img_file.read()).decode()
+    with open(image_path, "rb") as f:
+        img = base64.b64encode(f.read()).decode()
 
     st.markdown(
         f"""
         <style>
         .stApp {{
-            background-image: url("data:image/jpg;base64,{encoded}");
+            background-image: url("data:image/jpg;base64,{img}");
             background-size: cover;
             background-position: center;
         }}
 
         .login-box {{
-            background: rgba(0, 0, 0, 0.7);
+            background: rgba(0,0,0,0.75);
             padding: 30px;
-            border-radius: 12px;
+            border-radius: 15px;
             width: 420px;
             margin: auto;
             color: white;
