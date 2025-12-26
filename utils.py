5cee7e6fb18c7e7de9c42fa647f435ebb180d72c
Chaitanya081 <192424185.simats@saveetha.com>
2025-12-26 20:58:10 +0530

    utils.py
    Added a function to visualize job types and subscription outcomes.

--- utils.py
+++ utils.py
@@ -1,4 +1,13 @@
-import os
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-def get_image_path(filename):
-    return os.path.join(os.path.dirname(__file__), "images", filename)
+def show_graphs(df):
+    fig1, ax1 = plt.subplots()
+    df['job'].value_counts().head(5).plot(kind='bar', ax=ax1)
+    ax1.set_title("Top Job Types")
+    
+    fig2, ax2 = plt.subplots()
+    df['y'].value_counts().plot(kind='pie', autopct='%1.1f%%', ax=ax2)
+    ax2.set_title("Subscription Outcome")
+
+    return fig1, fig2
