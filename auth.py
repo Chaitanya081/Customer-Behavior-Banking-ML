e3bd2de9ae905f60a5903c470fb0ee4c9bbbdf43
Chaitanya081 <192424185.simats@saveetha.com>
2025-12-26 21:52:44 +0530

    auth.py

--- auth.py
+++ auth.py
@@ -1,31 +1,16 @@
 import streamlit as st
 
-def init_auth():
+def init_users():
     if "users" not in st.session_state:
         st.session_state.users = {}
-    if "logged_in" not in st.session_state:
-        st.session_state.logged_in = False
-    if "current_user" not in st.session_state:
-        st.session_state.current_user = None
-
 
 def register_user(email, password):
-    init_auth()
+    init_users()
     if email in st.session_state.users:
         return False
     st.session_state.users[email] = password
     return True
 
-
 def login_user(email, password):
-    init_auth()
-    if email in st.session_state.users and st.session_state.users[email] == password:
-        st.session_state.logged_in = True
-        st.session_state.current_user = email
-        return True
-    return False
-
-
-def logout_user():
-    st.session_state.logged_in = False
-    st.session_state.current_user = None
+    init_users()
+    return st.session_state.users.get(email) == password
